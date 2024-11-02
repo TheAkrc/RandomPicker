405cc9dbf5665918010c78441f46d33459921239
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2024-11-02 19:12:13 +0800

    修改为多线程http服务器
    修改为多线程http服务器；增加提示文本

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import tkinter as tk
 import webbrowser
 import threading
-import http.server
+from http.server import SimpleHTTPRequestHandler
+from http.server import CGIHTTPRequestHandler
+from http.server import ThreadingHTTPServer
+from functools import partial
+import contextlib
+import sys
+import os
 
+print("随机点名工具启动成功，请点击桌面左上角“点”按钮。")
 
-def http_server():
-    PORT = 5000
-    Handler = http.server.SimpleHTTPRequestHandler
-    with http.server.HTTPServer(("127.0.0.1", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        httpd.serve_forever()
+class DualStackServer(ThreadingHTTPServer):
+    def server_bind(self):
+        # suppress exception when protocol is IPv4
+        with contextlib.suppress(Exception):
+            self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+        return super().server_bind()
+
+def http_server(server_class=DualStackServer, handler_class=SimpleHTTPRequestHandler, port=5000, bind='127.0.0.1', cgi=False, directory=os.getcwd()):
+    if cgi:
+        handler_class = partial(CGIHTTPRequestHandler, directory=directory)
+    else:
+        handler_class = partial(SimpleHTTPRequestHandler, directory=directory)
+
+    with server_class((bind, port), handler_class) as httpd:
+        print(
+            f"Serving HTTP on {bind} port {port} "
+            f" (http://{bind}:{port}/) ..."
+        )
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received, exiting.")
+            sys.exit(0)
 
 
 threading.Thread(target=http_server).start()
@@ -27,7 +51,7 @@ root.overrideredirect(True)  # 去掉窗口边框
 root.attributes("-topmost", True)  # 确保窗口总在最上层
 
 # 设置不透明度为50%
-root.wm_attributes("-alpha", 0.5)
+root.wm_attributes("-alpha", 0.3)
 
 # 创建按钮
 button = tk.Button(root, text="点", command=open_url)
